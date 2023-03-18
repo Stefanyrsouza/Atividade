b2b776ac0e185c08c06f4b489e5b1f702b6250d2
Stefany Souza <stefanyraiissa24@gmail.com>
2023-03-18 00:30:37 -0300

    Atividade codificação

---
+++ Atividade/Pessoa_Fisica.cs
@@ -0,0 +1,8 @@
+namespace Atividade
+{
+    class Pessoa_Fisica : Clientes // Pessoa fisica está herdando da classe Clientes
+    {
+        public string cpf { get; set; }
+        public string rg { get; set; }
+    }
+}
